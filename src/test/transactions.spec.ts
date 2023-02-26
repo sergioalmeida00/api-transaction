3aca9772dab8b6149cdefe317fa04e92fbe22c40
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2023-02-26 00:03:16 -0300

    FRT: Adicionado teste no endpoint /transaction/:id

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -37,4 +37,33 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to get specific transaction', async () => {
+    const createTransactionResponse = await request(app)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 3000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('set-cookie')
+
+    const listTransactionsResponse = await request(app)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const idTransaction =
+      listTransactionsResponse.body.newTransactionsFormat[0].id
+
+    const getTransaction = await request(app)
+      .get(`/transactions/${idTransaction}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransaction.body.transaction).toHaveProperty('id')
+    expect(getTransaction.body.transaction).toHaveProperty('title')
+    expect(getTransaction.body.transaction).toHaveProperty('amount')
+    expect(getTransaction.body.transaction).toHaveProperty('session_id')
+  })
 })
