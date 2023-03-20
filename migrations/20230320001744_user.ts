1093510757d6197a0270a9725bf744fc4ce9581e
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2023-03-19 21:41:55 -0300

    FEAT: Add migration relation transaction with users

---
+++ migrations/20230320001744_user.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.uuid('user_id').unsigned()
+    table.foreign('user_id').references('id').inTable('users')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropForeign('user_id')
+    table.dropColumn('user_id')
+  })
+}
